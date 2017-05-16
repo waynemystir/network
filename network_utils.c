c81dcc9290840d1bbf9962d5ccd15d2a44edfe50
Wayne Small <waynemystir@gmail.com>
2017-05-16 16:13:10 -0500

    push bump

--- network_utils.c
+++ network_utils.c
@@ -227,6 +227,7 @@ int get_if_addr_iOS_OSX(IF_ADDR_PREFFERED iap,
 	struct sockaddr **ret_addr,
 	size_t *size_addr,
 	char ip_str[INET6_ADDRSTRLEN]) {
+	// just a git push bump
 	if (!ret_addr || !size_addr) {
 		printf("A NULL sockaddr** or size_addr was given to str_to_addr\n");
 		return -1;
